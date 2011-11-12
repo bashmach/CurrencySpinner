8b7a178a4aefc277ea2d3b60700ae50658bf45b3
Pavel Machekhin <pavel.machekhin@gmail.com>
2011-11-12 16:03:16 +0200

    Added summary and description

--- CurrencySpinner.js
+++ CurrencySpinner.js
@@ -4,6 +4,12 @@ define([
         "dijit/form/CurrencyTextBox"
     ],
     function(declare, NumberSpinner, CurrencyTextBox) {
+        
+        // summary:
+		//		A validating currency spinner
+		// description:
+		//		CurrencySpinner is a mixin of `dijit.form.NumberSpinner` and `dijit.form.CurrencyTextBox`
+        
         return declare("app.form.CurrencySpinner.CurrencySpinner", [NumberSpinner, CurrencyTextBox], {
             baseClass: "dijitTextBox dijitCurrencyTextBox dijitSpinner"
         });
---
+++ README.md
@@ -0,0 +1,9 @@
+# CurrencySpinner
+
+## Summary
+
+A validating currency spinner
+
+## Description
+
+The Dojo Toolkit widget that mixes dijit.form.CurrencyTextBox and dijit.form.NumberSpinner
